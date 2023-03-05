b68281f4f4aaf7dfd7042056f41292066789437b
Libby Lewis <elewis43@jh.edu>
2023-03-05 15:44:46 -0600

    Screen state

--- app/src/main/java/lewis/libby/module4_example/MovieViewModel.kt
+++ app/src/main/java/lewis/libby/module4_example/MovieViewModel.kt
@@ -14,14 +14,41 @@ sealed interface Screen
 object RatingList: Screen
 object MovieList: Screen
 object ActorList: Screen
-data class RatingScreen(
+
+data class RatingDisplay(
+    val id: String
+): Screen
+data class ActorDisplay(
+    val id: String
+): Screen
+data class MovieDisplay(
     val id: String
 ): Screen
 
 class MovieViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: MovieRepository = MovieDatabaseRepository(application)
 
-    var screen by mutableStateOf<Screen>(RatingList)
+    private var screenStack: List<Screen> = listOf(MovieList)
+        set(value) {
+            field = value
+            screen = value.lastOrNull()
+        }
+
+    fun pushScreen(screen: Screen) {
+        screenStack = screenStack + screen
+    }
+
+    fun popScreen() {
+        if (screenStack.isNotEmpty()) {
+            screenStack = screenStack.dropLast(1)
+        }
+    }
+
+    fun setScreenStack(screen: Screen) {
+        screenStack = listOf(screen)
+    }
+
+    var screen by mutableStateOf<Screen?>(MovieList)
         private set
 
     val ratingsFlow = repository.ratingsFlow
