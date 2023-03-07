352181caf56a01b3b2590854cbcf01b197ec0632
Libby Lewis <elewis43@jh.edu>
2023-03-07 16:24:56 -0600

    Referential integrity

--- data/src/main/java/lewis/libby/module5_example/data/Role.kt
+++ data/src/main/java/lewis/libby/module5_example/data/Role.kt
@@ -1,9 +1,31 @@
 package lewis.libby.module5_example.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 
 @Entity(
     primaryKeys = ["actorId", "movieId"],
+    indices = [
+        Index(value = ["movieId"]),
+        Index(value = ["actorId"]),
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = Actor::class,
+            parentColumns = ["id"],
+            childColumns = ["actorId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE,
+        ),
+        ForeignKey(
+            entity = Movie::class,
+            parentColumns = ["id"],
+            childColumns = ["movieId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ]
 )
 data class Role(
     var movieId: String,
